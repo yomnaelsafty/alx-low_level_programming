16026103a8bf03fa7f4d023ca2c86287d624c3a0
Yomna <yomna421@gmail.com>
2023-08-16 11:44:57 -0700

    p

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print _alphabet - utilizes on the _putchar function to print
+ * print_alphabet - utilizes on the _putchar function to print
  *                  the alphabet a - z
 */
 
