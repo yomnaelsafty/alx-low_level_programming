3f0a870057c2a0583f6e23319307f6ccebd72dc3
Yomna <yomna421@gmail.com>
2023-08-21 10:49:53 -0700

    p

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,7 +2,7 @@
 /**
  * print_rev -  prints a string, in reverse, followed by a new line
  *
- * @s: parameter to the string to print
+ * @s: pointer to the string to print
  *
  * Return: Nothing
 */
