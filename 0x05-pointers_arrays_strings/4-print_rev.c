17ab47596f25cb3ea023c65a3e5a4a1dc3b9e46b
Yomna <yomna421@gmail.com>
2023-08-21 10:46:35 -0700

    p

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,8 +1,10 @@
 #include "main.h"
 /**
  * print_rev -  prints a string, in reverse, followed by a new line
+ *
  * @s: parameter to the string to print
- * Return: void
+ *
+ * Return: Nothing
 */
 void print_rev(char *s)
 {
