cc7dd1212569c4d2fc398a8956c2868f9c59e61a
Yomna <yomna421@gmail.com>
2023-08-28 16:31:44 -0700

    p

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * *_strchr -  locates a character in a string.
+ * @s: pointer to put the constant
+ * @c: constant
+ * @Return pointer to s
+*/
+char *_strchr(char *s, char c)
+{
+	int itr;
+
+	for (itr = 0; s[itr] >= '\0' ; itr++)
+	{
+		if (s[itr] == c)
+		{
+			return (s + itr);
+		}
+	}
+
+	return ('\0');
+}
