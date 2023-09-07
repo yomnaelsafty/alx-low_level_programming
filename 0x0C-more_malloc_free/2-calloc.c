9ebb0f5df93de0ea301c530552ae21e526c239d8
Yomna <yomna421@gmail.com>
2023-09-07 09:50:48 -0700

    p

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -27,6 +27,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (size == 0 || nmemb == 0)
 		return (NULL);
+	m = malloc(sizeof(int) * nmemb);
+
+	if (m == 0)
+		return (NULL);
 
 	_memset(m, 0, sizeof(int) * nmemb);
 
