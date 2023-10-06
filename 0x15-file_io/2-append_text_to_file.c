224acf652e8b21fe44308d93a6be9d3c127012ee
yomna <yomna421@gmail.com>
2023-10-06 06:32:35 -0700

    p

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -36,7 +36,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	if (len)
-		bytes = writ(fd, text_content, len);
+		bytes = write(fd, text_content, len);
 	close(fd);
 	return (bytes == len ? 1 : -1);
 }
