cf60bca4e20980dc22a5e6c7f794bae456647298
yomna <yomna421@gmail.com>
2023-10-06 07:11:16 -0700

    p

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-#define USAGE "usage: cp file_from file_to\n"
+#define USAGE "Usage: cp file_from file_to\n"
 #define ERR_NOREAD "Error: Can't read from file %s\n"
 #define ERR_NOWRITE "Error: Can't write to %s\n"
 #define ERR_NOCLOSE "Error: Can't close fd %d\n"
